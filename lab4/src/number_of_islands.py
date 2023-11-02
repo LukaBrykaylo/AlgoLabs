96ec1ef4dea015b4572fdd337530c841c6861ea7
Luka Brykaylo <lukabrykaylo@gmail.com>
2023-11-02 21:10:23 +0200

    adding bfs

--- lab4/src/number_of_islands.py
+++ lab4/src/number_of_islands.py
@@ -1,25 +1,33 @@
+from collections import deque
+
+
 def number_of_islands(island_map):
     if not island_map:
         return 0
-    number = 0
+    
+    num_islands = 0
+
     for i in range(len(island_map)):
         for j in range(len(island_map[0])):
-            if (island_map[i][j] == 1):
-                number += 1
-                dfs(i, j, island_map)
-    return number
+            if island_map[i][j] == 1:
+                num_islands += 1
+                bfs(i, j, island_map)
+
+    return num_islands
 
 
-def dfs(i, j, island_map):
-    if i < 0 or j < 0 or i >= len(island_map) or j >= len(island_map[0]) or island_map[i][j] == 0:
-        return 0
+def bfs(i, j, island_map):
+    queue = deque()
+    queue.append((i, j))
     island_map[i][j] = 0
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1),
+                  (1, 1), (-1, -1), (1, -1), (-1, 1)]
 
-    dfs(i+1, j, island_map)
-    dfs(i - 1, j, island_map)   
-    dfs(i, j + 1, island_map)
-    dfs(i, j - 1, island_map)
-    dfs(i+1, j+1, island_map)
-    dfs(i+1, j-1, island_map)
-    dfs(i-1, j+1, island_map)
-    dfs(i-1, j-1, island_map)
+    while queue:
+        x, y = queue.popleft()
+
+        for dirx, diry in directions:
+            upd_x, upd_y = x + dirx, y + diry
+            if 0 <= upd_x < len(island_map) and 0 <= upd_y < len(island_map[0]) and island_map[upd_x][upd_y] == 1:
+                queue.append((upd_x, upd_y))
+                island_map[upd_x][upd_y] = 0
